dd2d19030097f33ff2a48600f07bf78f359ad483
Mateusz Romański <mromanski@concisesoftware.com>
2022-08-16 09:43:57 +0200

    refactor notification to be more store depended

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,11 @@ import Notification from "./components/Notification";
 import { useReduxHooks } from "./hooks/useReduxHooks";
 import { RootState } from "./store";
 
-const App: FC = () => {
-  const { useAppSelector } = useReduxHooks();
-
-  const notification = useAppSelector((state: RootState) => state.notification);
-
-  return (
-    <div>
-      <ControlPanel />
-      {notification.show && (
-        <Notification type={notification.type} message={notification.message} />
-      )}
-    </div>
-  );
-};
+const App: FC = () => (
+  <>
+    <ControlPanel />
+    <Notification />
+  </>
+);
 
 export default App;
--- src/components/Notification/Notification.types.ts
+++
@@ -1,6 +0,0 @@
-import { ENotificationTypes } from "../../interfaces/notification.interface";
-
-export interface INotificationProps {
-  type: ENotificationTypes;
-  message: string;
-}
